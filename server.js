c0c9717348097ab3b1a078ab9579067c91f2c275
choia4 <49245467+choia4@users.noreply.github.com>
2019-10-21 16:29:29 -0700

    make it better

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = require('./config/keys').mongoURI;
 
 //Connect to Mongo
 mongoose
-    .connect(db)
+    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
@@ -26,18 +26,13 @@ app.use('/api/items', items);
 //serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
     //Set static folder
+    console.log("inside here");
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
-
-const indexPath = path.join(__dirname, './index.html')
-const distPath = express.static(path.join(__dirname, './dist'))
-
-app.use('/dist', distPath)
-app.get('/', function (req, res) { res.sendFile(indexPath) }) 
 
 const port = process.env.PORT || 5000;
 
