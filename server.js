c6346bc321c2a3cdf5fb9948871c5d203748f702
choia4 <49245467+choia4@users.noreply.github.com>
2019-10-21 16:04:00 -0700

    make it better

--- server.js
+++ server.js
@@ -33,6 +33,12 @@ if(process.env.NODE_ENV === 'production') {
     });
 }
 
+const indexPath = path.join(__dirname, './index.html')
+const distPath = express.static(path.join(__dirname, './dist'))
+
+app.use('/dist', distPath)
+app.get('/', function (req, res) { res.sendFile(indexPath) }) 
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log( `Server started on port ${port}`));
